761a6e6d4202be76649f5532638a508e83019236
Buhay Malaya <buhaymalaya@Buhays-Air.lan>
2024-01-22 10:26:32 -0500

    After tutorial

---
+++ module.py
@@ -0,0 +1,14 @@
+import math
+
+# Module to find the square footage of a house
+
+def area(length,width):
+    area = int(length * width)
+    return area
+
+# Module to find the circumference of a circle
+
+def circumference(r):
+    circumference = 2 * math.pi * r
+    return circumference
+
